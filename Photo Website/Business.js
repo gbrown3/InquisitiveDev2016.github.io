b9960d0a38c0c2f851644e81e43dcab145b4398c
InquisitiveDev2016 <alihaider2011@live.ca>
2018-02-21 21:56:46 -0500

    Update Business.js

--- Photo Website/Business.js
+++ Photo Website/Business.js
@@ -1,10 +1,10 @@
 $(function() {
-  console.log('ready');
-  $('a[href^=#]').on('click', function(e) {
-    console.log('click');
-    e.preventDefault();
-    $('html, body').animate({
-      scrollTop: $($(this).attr('href')).offset().top
-    }, 500, 'linear');
-  });
+    console.log('ready');
+    $('a[href^=#]').on('click', function(e) {
+        console.log('click');
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
 });
